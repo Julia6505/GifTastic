f8477a56b46363bae4831d932ed7d168ffecc167
Julia McGee <JuliaMcGee@Kenneths-MacBook-Pro.local>
2017-09-03 20:19:44 -0400

    working on ajax

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -4,13 +4,24 @@ var topics = ["New York", "Orlando", "Boston", "San Francisco", "Chicago", "Sant
   
 
 function displayGifs() {
-  var city = $(this).attr("data-buttons")
+  var city = $(this).attr("data-buttons");
   var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + city + "&api_key=17b05d17dc0d4bcdbf4b1156a3cbded9&limit=10";
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).done(function(response) {
         console.log(response);
+        var gifDiv = $("<div class='gif-div'>");
+        for(var i = 0; i < response.data.length; i++) {
+        var rating = response.data[i].rating;
+        var ratingP = $("<p>").text("Rating: " + rating);
+        // console.log(rating);
+        var gifImageUrl = response.data[i].embed_url;
+        // var gifImageUrl = response.data[i].bitly_gif_url;
+        var gifImage = $("<img>").attr("src", gifImageUrl);
+        console.log(gifImageUrl);
+    };
+        
     });
 };
 
@@ -19,13 +30,12 @@ function displayButtons() {
     $("#view-buttons").empty();
 
     for(var i = 0; i < topics.length; i++) {
-
-
     var addButton = $("<button>");
     addButton.addClass("cities");
     addButton.attr("data-buttons", topics[i]);
     addButton.text(topics[i]);
     $("#view-buttons").append(addButton);
+    // console.log(addButton.attr("data-buttons", topics[i]));
     }
 };
 
@@ -40,4 +50,6 @@ $("#submit-city").on("click", function(event){
 
 displayButtons();   
 console.log(topics);
+
+
 
